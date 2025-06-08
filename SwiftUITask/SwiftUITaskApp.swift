c0afa9dc703ad44a1625a18ff5f27dec791b35a2
Prashant7026 <prashantkumarsoni30@gmail.com>
2025-06-08 15:15:20 +0530

    Initial Commit

---
+++ SwiftUITask/SwiftUITaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITaskApp.swift
+//  SwiftUITask
+//
+//  Created by Prashant Kumar Soni on 08/06/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
