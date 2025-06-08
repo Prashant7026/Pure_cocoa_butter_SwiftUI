bab43730951089887be2b8375f4914881511d035
Prashant7026 <prashantkumarsoni30@gmail.com>
2025-06-08 16:36:54 +0530

    Added history icon

--- SwiftUITask/TabBarView.swift
+++ SwiftUITask/TabBarView.swift
@@ -27,7 +27,7 @@ struct TabBarView: View {
             Divider()
             
             HStack {
-                tabBarItem(index: 0, label: "History", systemImage: "clock.arrow.circlepath", tint: .green)
+                tabBarItem(index: 0, label: "History", systemImage: "historyIcon", tint: .green, isAssetImage: true)
                 tabBarItem(index: 1, label: "Recs", systemImage: "arrow.left.arrow.right.circle", tint: .green)
                 tabBarItem(index: 2, label: "Scan", systemImage: "barcode.viewfinder", tint: .green)
                 tabBarItem(index: 3, label: "Top", systemImage: "list.star", tint: .green)
@@ -38,15 +38,24 @@ struct TabBarView: View {
         }
     }
     
-    func tabBarItem(index: Int, label: String, systemImage: String, tint: Color) -> some View {
+    func tabBarItem(index: Int, label: String, systemImage: String, tint: Color, isAssetImage: Bool = false) -> some View {
         Button {
             selectedTab = index
         } label: {
             VStack {
-                Image(systemName: systemImage)
-                    .resizable()
-                    .foregroundColor(selectedTab == index ? tint : .gray)
-                    .frame(width: 25, height: 25)
+                Group {
+                    if isAssetImage {
+                        Image(systemImage)
+                            .resizable()
+                            .renderingMode(.template)
+                            .foregroundColor(selectedTab == index ? tint : .black)
+                    } else {
+                        Image(systemName: systemImage)
+                            .resizable()
+                            .foregroundColor(selectedTab == index ? tint : .gray)
+                    }
+                }
+                .frame(width: 25, height: 25)
                 Text(label)
                     .foregroundColor(selectedTab == index ? tint : .gray)
                     .font(.caption)
